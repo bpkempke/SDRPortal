6a346c5224ba16c9c65461fc025f016bb5fc60d3
Ben Kempke <bpkempke@umich.edu>
2013-06-16 20:08:44 -0400

    ready to test the websocket sink

--- gr-sdrportal/lib/ws_sink_c_impl.cc
+++ gr-sdrportal/lib/ws_sink_c_impl.cc
@@ -25,47 +25,65 @@
 #include <gnuradio/io_signature.h>
 #include <string>
 #include "ws_sink_c_impl.h"
-#include "genericSocketInterface.h"
 
 namespace gr {
-  namespace sdrportal {
+namespace sdrportal {
 
-    ws_sink_c::sptr
-    ws_sink_c::make(bool is_server, int portnum, std::string otw_format, std::string address)
-    {
-      return gnuradio::get_initial_sptr
-        (new ws_sink_c_impl(is_server, portnum, otw_format, address));
-    }
+ws_sink_c::sptr	ws_sink_c::make(bool is_server, int portnum, std::string otw_format, std::string address) {
+		return gnuradio::get_initial_sptr
+			(new ws_sink_c_impl(is_server, portnum, otw_format, address));
+}
 
-    /*
-     * The private constructor
-     */
-    ws_sink_c_impl::ws_sink_c_impl(bool is_server, int portnum, std::string otw_format, std::string address)
-      : gr::sync_block("ws_sink_c",
-              gr::io_signature::make(1, 1, sizeof(gr_complex)),
-              gr::io_signature::make(0, 0, 0))
-    {}
+/*
+ * The private constructor
+ */
+ws_sink_c_impl::ws_sink_c_impl(bool is_server, int portnum, std::string otw_format, std::string address)
+	: gr::sync_block("ws_sink_c",
+			gr::io_signature::make(1, 1, sizeof(gr_complex)),
+			gr::io_signature::make(0, 0, 0)){
+	if(is_server){
+		sock_int = new genericSocketInterface(SOCKET_WS, portnum);
+		sock_int->addUpperLevel(this);
+		this->addLowerLevel(sock_int);
+	}else{
+		//TODO: Add in functionality for WS client socket
+	}
 
-    /*
-     * Our virtual destructor.
-     */
-    ws_sink_c_impl::~ws_sink_c_impl()
-    {
-    }
+	if(otw_format == "DOUBLE")
+		result_type = DOUBLE;
+	else if(otw_format == "FLOAT")
+		result_type = FLOAT;
+	else if(otw_format == "INT32")
+		result_type = INT32;
+	else if(otw_format == "INT16")
+		result_type = INT16;
+	else if(otw_format == "INT8")
+		result_type = INT8;
+}
 
-    int
-    ws_sink_c_impl::work(int noutput_items,
-			  gr_vector_const_void_star &input_items,
-			  gr_vector_void_star &output_items)
-    {
-        const gr_complex *in = (const gr_complex *) input_items[0];
+/*
+ * Our virtual destructor.
+ */
+ws_sink_c_impl::~ws_sink_c_impl(){
+}
+	
+int ws_sink_c_impl::work(int noutput_items, gr_vector_const_void_star &input_items, gr_vector_void_star &output_items){
 
-        // Do <+signal processing+>
+	const float *in = (const float *) input_items[0];
 
-        // Tell runtime system how many output items we produced.
-        return noutput_items;
-    }
+	//Convert everything to the requested result type
+	stream_conv.convertTo((float*)in, noutput_items*2, result_type);
 
-  } /* namespace sdrportal */
+	//Send everything out to the socket
+	messageType resulting_message;
+	resulting_message.buffer = (char*)stream_conv.getResult();
+	resulting_message.num_bytes = noutput_items*sizeof(float)*2;
+	dataToLowerLevel(stream_conv.getResult(), 1);
+
+	// Tell runtime system how many output items we produced.
+	return noutput_items;
+}
+
+} /* namespace sdrportal */
 } /* namespace gr */
 
--- gr-sdrportal/lib/ws_sink_c_impl.h
+++ gr-sdrportal/lib/ws_sink_c_impl.h
@@ -22,26 +22,32 @@
 #define INCLUDED_SDRPORTAL_WS_SINK_C_IMPL_H
 
 #include <sdrportal/ws_sink_c.h>
+#include "genericSocketInterface.h"
+#include "streamConverter.h"
 
 namespace gr {
-  namespace sdrportal {
+namespace sdrportal {
 
-    class ws_sink_c_impl : public ws_sink_c
-    {
-     private:
-      // Nothing to declare in this block.
+class ws_sink_c_impl : public ws_sink_c, hierarchicalDataflowBlock {
+private:
+	// Nothing to declare in this block.
+	genericSocketInterface *sock_int;
+	streamConverter<float> stream_conv;
+	primType result_type;
 
-     public:
-      ws_sink_c_impl(bool is_server, int portnum, std::string otw_format, std::string address);
-      ~ws_sink_c_impl();
+public:
+	ws_sink_c_impl(bool is_server, int portnum, std::string otw_format, std::string address);
+	~ws_sink_c_impl();
 
-      // Where all the action really happens
-      int work(int noutput_items,
-	       gr_vector_const_void_star &input_items,
-	       gr_vector_void_star &output_items);
-    };
+	// Where all the action really happens
+	int work(int noutput_items,
+			gr_vector_const_void_star &input_items,
+			gr_vector_void_star &output_items);
+	virtual void dataFromUpperLevel(void *data, int num_bytes, int local_up_channel=0){};
+	virtual void dataFromLowerLevel(void *data, int num_bytes, int local_down_channel=0){};
+};
 
-  } // namespace sdrportal
+} // namespace sdrportal
 } // namespace gr
 
 #endif /* INCLUDED_SDRPORTAL_WS_SINK_C_IMPL_H */
