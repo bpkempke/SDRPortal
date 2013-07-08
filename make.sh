d2df31c6ae23d6f7da38e3c610ddf51c2bd49827
Ben Kempke <bpkempke@umich.edu>
2013-07-08 14:00:29 -0400

    linking shared libraries more efficiently and getting rid of committed automake stuff

--- make.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NON_MAIN_FILES=`ls src/*.cc | grep -v _`
-echo $NON_MAIN_FILES
-g++ -std=c++0x -Wall -g -pthread -Ilib $NON_MAIN_FILES src/uhd_daemon.cc -luhd -lrtlsdr -lhackrf -o ./bin/uhdd
-g++ -std=c++0x -Wall -g -pthread -Ilib $NON_MAIN_FILES src/shell_daemon.cc -luhd -lrtlsdr -lhackrf -o ./bin/shelld
