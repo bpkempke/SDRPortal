88fd2b129f64f755689b753bfe360e37f061f897
Ben Kempke <bpkempke@umich.edu>
2013-07-17 22:26:37 -0400

    done recoding streamConverter

--- lib/streamConverter.h
+++ lib/streamConverter.h
@@ -1,7 +1,7 @@
 #ifndef STREAM_CONVERTER_H
 #define STREAM_CONVERTER_H
 
-#include "generic.h"
+#include "streamConversionHelper.h"
 #include <algorithm>
 #include <map>
 
@@ -14,138 +14,30 @@ public:
 	scratchspaceType() : scratchspace(NULL), cur_capacity(0), cur_size(0) {};
 };
 
-template <typename T>
+struct fullConvType{
+	convFunc cnv_to_common;
+	convFunc cnv_from_common;
+};
+
 class streamConverter {
 public:
-	streamConverter();
-	int convertTo(T *in_data, int num_bytes, primType result_type);
-	int convertFrom(void *in_data, int num_bytes, primType start_type);
-	void *getResult();
+	streamConverter(streamType common_type);
+	void setConversionToFrom(streamType from_type, streamType to_type);
+	int convertToCommon(void *in_data, int num_bytes, streamType start_type, int num_prims_per_block);
+	int convertFromCommon(void *in_data, int num_bytes, streamType result_type, int num_prims_per_block);
 private:
-	int sizeOfType(primType in_type);
+	int convertWorker(void *in_data, int num_bytes, streamType secondary_type, int num_prims_per_block, bool convert_to);
 
 	//Scratchspace used in convertFrom()
 	scratchspaceType t_scratch;
 
 	//Current type points to the last type sent to convertTo()
-	primType cur_type;
+	streamType common_type;
 
 	//Map to different scratchspaces used in convertTo()
-	std::map<primType, scratchspaceType> scratchspace_map;
+	std::map<streamType, scratchspaceType> scratchspace_map;
+	std::map<streamType, fullConvType> conversion_map;
 };
-
-template <typename T>
-streamConverter<T>::streamConverter(){
-}
-
-template <typename TO, typename FROM>
-int convertWorker(scratchspaceType &in_scratch, FROM *in_from_array, int num_bytes){
-	//First count the number of actual elements
-	in_scratch.cur_size = num_bytes/sizeof(FROM);
-
-	//Resize the desired array if necessary
-	if(in_scratch.cur_size > in_scratch.cur_capacity){
-		delete [] (TO*)in_scratch.scratchspace;
-		in_scratch.scratchspace = new TO[in_scratch.cur_size*2];
-		in_scratch.cur_capacity = in_scratch.cur_size*2;
-	}
-
-	//Do a standard copy and type conversion
-	std::copy(in_from_array, in_from_array+in_scratch.cur_size, (TO*)(in_scratch.scratchspace));
-
-	//Return the number of resulting bytes
-	return in_scratch.cur_size*sizeof(TO);
-}
-
-template <typename T>
-int streamConverter<T>::convertTo(T *in_data, int num_bytes, primType result_type){
-	int resulting_bytes;
-	cur_type = result_type;
-	
-	switch(result_type){
-		case DOUBLE:
-			resulting_bytes = convertWorker<double, T>(scratchspace_map[result_type], in_data, num_bytes);
-			break;
-
-		case FLOAT:
-			resulting_bytes = convertWorker<float, T>(scratchspace_map[result_type], in_data, num_bytes);
-			break;
-
-		case INT32:
-			resulting_bytes = convertWorker<int32_t, T>(scratchspace_map[result_type], in_data, num_bytes);
-			break;
-
-		case INT16:
-			resulting_bytes = convertWorker<int16_t, T>(scratchspace_map[result_type], in_data, num_bytes);
-			break;
-
-		case INT8:
-			resulting_bytes = convertWorker<int8_t, T>(scratchspace_map[result_type], in_data, num_bytes);
-			break;
-
-		default:
-			//SHOULD NOT GET HERE!
-			break;
-	}
-
-	return resulting_bytes;
-}
-
-template <typename T>
-int streamConverter<T>::convertFrom(void *in_data, int num_bytes, primType start_type){
-	//First count the number of actual elements
-	int num_elements = num_bytes/sizeOfType(start_type);
-
-	int resulting_bytes = num_elements*sizeof(T);
-	if(num_elements > t_scratch.cur_capacity){
-		delete [] t_scratch.scratchspace;
-		t_scratch.scratchspace = new T[num_elements*2];
-		t_scratch.cur_capacity = num_elements*2;
-	}
-
-	switch(start_type){
-		case DOUBLE:
-			std::copy((double*)in_data, ((double*)in_data) + num_elements, t_scratch.scratchspace);
-			break;
-
-		case FLOAT:
-			std::copy((float*)in_data, ((float*)in_data) + num_elements, t_scratch.scratchspace);
-			break;
-
-		case INT32:
-			std::copy((int32_t*)in_data, ((int32_t*)in_data) + num_elements, t_scratch.scratchspace);
-			break;
-
-		case INT16:
-			std::copy((int16_t*)in_data, ((int16_t*)in_data) + num_elements, t_scratch.scratchspace);
-			break;
-
-		case INT8:
-			std::copy((int8_t*)in_data, ((int8_t*)in_data) + num_elements, t_scratch.scratchspace);
-			break;
-	}
-
-	return resulting_bytes;
-}
-
-template <typename T>
-int streamConverter<T>::sizeOfType(primType in_type){
-	if(in_type == DOUBLE)
-		return sizeof(double);
-	else if(in_type == FLOAT)
-		return sizeof(float);
-	else if(in_type == INT32)
-		return sizeof(int32_t);
-	else if(in_type == INT16)
-		return sizeof(int16_t);
-	else if(in_type == INT8)
-		return sizeof(int8_t);
-}
-
-template <typename T>
-void *streamConverter<T>::getResult(){
-	return scratchspace_map[cur_type].scratchspace;
-}
 
 #endif
 
---
+++ src/streamConverter.cc
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <map>
+
+streamConverter::streamConverter(streamType common_type){
+	this->common_type = common_type;
+}
+
+void streamConverter::setConversionCommonType(streamType common_type){
+	this->common_type = common_type;
+
+	//Update conversion function map
+	for(std::map<streamType, convFunc>::iterator it=conversion_map.begin(); it != 	conversion_map.end(); it++){
+		conversion_map[it->first].cnv_to_common = getConversionFunc(it->first, common_type);
+		conversion_map[it->first].cnv_from_common = getConversionFunc(common_type, it->first);
+	}
+}
+
+int streamConverter::convertToCommon(void *in_data, int num_bytes, primType start_type, int num_prims_per_block){
+	return convertWorker(in_data, num_bytes, secondary_type, num_prims_per_block, true);;
+}
+
+int streamConverter::convertFromCommon(void *in_data, int num_bytes, primType result_type, int num_prims_per_block){
+	return convertWorker(in_data, num_bytes, secondary_type, num_prims_per_block, false);;
+}
+
+int streamConverter::convertWorker(void *in_data, int num_bytes, streamType secondary_type, int num_prims_per_block, bool convert_to){
+	int stream_t_len = (convert_to) ? getStreamTypeLength(secondary_type) : getStreamTypeLength(common_type);
+
+	//Only process the number of bytes which can be formed into a block
+	int num_translated_blocks = num_bytes/num_prims_per_block/stream_t_len;
+	int num_consumed_bytes = num_translated_blocks*num_prims_per_block*stream_t_len;
+
+	//Have to convert to char* in order to do arithmetic on end pointer
+	char *end_pointer = (char*)in_data;
+
+	//Make sure there's a scratchspace available
+	if(scratchspace_map.find(secondary_type) == scratchspace_map.end() || scratchspace_map[secondary_type].cur_capacity < num_consumed_bytes){
+
+		delete [] scratchspace_map[secondary_type].scratchspace;
+		scratchspace_map[secondary_type].scratchspace = new char[num_consumed_bytes];
+		scratchspace_map[secondary_type].cur_capacity = num_consumed_bytes;
+
+		//Set up the conversion functions if necessary
+		if(conversion_map.find(secondary_type) == conversion_map.end()){
+			conversion_map[secondary_type].cnv_to_common = getConversionFunc(secondary_type, common_type);
+			conversion_map[secondary_type].cnv_from_common = getConversionFunc(common_type, secondary_type);
+		}
+	}
+	
+	//Call the actual conversion function
+	if(convert_to)
+		conversion_map[secondary_type].cnv_to_common(in_data, (void*)(end_pointer+num_consumed_bytes), scratchspace_map[secondary_type].scratchspace);
+	else
+		conversion_map[secondary_type].cnv_from_common(in_data, (void*)(end_pointer+num_consumed_bytes), scratchspace_map[secondary_type].scratchspace);
+
+	return num_consumed_bytes;
+}
+
